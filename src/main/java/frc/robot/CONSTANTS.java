aacd3c1ca38e3c14e1883e70070ab33279f8cb36
Mahdi <mahdi.alhassani@icloud.com>
2025-03-19 14:04:56 -0700

    Wrist things need to do

--- src/main/java/frc/robot/CONSTANTS.java
+++ src/main/java/frc/robot/CONSTANTS.java
@@ -539,20 +539,20 @@ public final class CONSTANTS {
             WRIST_CONFIG.Feedback.FeedbackSensorSource = FeedbackSensorSourceValue.RemoteCANcoder;
             WRIST_CONFIG.Feedback.RotorToSensorRatio = WRIST_GEAR_RATIO;
 
-            WRIST_CONFIG.Slot0.kG = 0.0; // Volts to overcome gravity
-            WRIST_CONFIG.Slot0.kS = 0.3; // Volts to overcome static friction
-            WRIST_CONFIG.Slot0.kV = 0.3; // Volts for a velocity target of 1 rps
-            WRIST_CONFIG.Slot0.kA = 0.01; // Volts for an acceleration of 1 rps/s
-            WRIST_CONFIG.Slot0.kP = 35;
+            WRIST_CONFIG.Slot0.kG = 0.1; // Volts to overcome gravity
+            WRIST_CONFIG.Slot0.kS = 0.2; // Volts to overcome static friction
+            WRIST_CONFIG.Slot0.kV = 0.4; // Volts for a velocity target of 1 rps
+            WRIST_CONFIG.Slot0.kA = 0.02; // Volts for an acceleration of 1 rps/s
+            WRIST_CONFIG.Slot0.kP = 30;
             WRIST_CONFIG.Slot0.kI = 0;
-            WRIST_CONFIG.Slot0.kD = 0.8;
+            WRIST_CONFIG.Slot0.kD = 0;
 
             WRIST_CONFIG.Slot0.GravityType = GravityTypeValue.Arm_Cosine;
             // WRIST_CONFIG.Slot0.StaticFeedforwardSign =
             // StaticFeedforwardSignValue.UseClosedLoopSign;
 
-            WRIST_CONFIG.MotionMagic.MotionMagicCruiseVelocity = 50;
-            WRIST_CONFIG.MotionMagic.MotionMagicAcceleration = 2100;
+            WRIST_CONFIG.MotionMagic.MotionMagicCruiseVelocity = 100;
+            WRIST_CONFIG.MotionMagic.MotionMagicAcceleration = 20;
         }
         public static final Angle PIVOT_INTAKE_CORAL = Units.Degrees.of(-70);
         public static final Angle PIVOT_ALGAE_GROUND = Units.Degrees.of(30);
